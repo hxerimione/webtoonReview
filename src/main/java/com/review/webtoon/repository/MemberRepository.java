dc3d28c117a9519a480c483f1f8ce15b7db75560
hxerimione <purejang98@gmail.com>
2023-02-24 23:52:55 +0900

    update member api

--- src/main/java/com/review/webtoon/repository/MemberRepository.java
+++ src/main/java/com/review/webtoon/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package com.review.webtoon.repository;
 
 import com.review.webtoon.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member,Long> {
@@ -16,4 +19,8 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 
     @Query("select u from Member u join fetch u.reviews where u.username = :username")
     Member findByUsernameWithReviews(@Param("username") String username);
+
+    @EntityGraph(attributePaths = "reviews")
+    Optional<Member> findMemberEntityGraphByUsername(@Param("username") String username);
+
 }
--- src/main/java/com/review/webtoon/service/MemberService.java
+++ src/main/java/com/review/webtoon/service/MemberService.java
@@ -5,11 +5,17 @@ import com.review.webtoon.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     public Long saveUser(Member member){
         return memberRepository.save(member).getId();
+    }
+    public Optional<Member> findUserByEntityManager(String username){
+        return memberRepository.findMemberEntityGraphByUsername(username);
+
     }
 }
